948f06f4dc236851c4efe398125a2251e2c846dc
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-02-13 18:02:57 +0700

    Complete

---
+++ e-wallet/src/components/Card/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div className="card">
+      <div>
+        <img src="/src/assets/chip-dark-svg" />
+      </div>
+    </div>
+  );
+}
--- e-wallet/src/components/Home/Home.jsx
+++ e-wallet/src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import "./Home.css";
 import Top from "../Top/Top";
+import Card from "../Card/Card";
 
 export default function Home() {
   return (
     <div className="home">
       <Top />
+      <Card />
       <button className="home-btn">ADD A NEW CARD</button>
     </div>
   );
