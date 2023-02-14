507675e59f2dab55cfe1dcd1747681c8bccbdad9
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-02-14 23:09:19 +0700

    Complete

--- e-wallet/src/components/Top/Top.jsx
+++ e-wallet/src/components/Top/Top.jsx
@@ -1,6 +1,13 @@
 import "./Top.css";
 
 export default function Top() {
+  const addCardHeader = {
+    title: "ADD A NEW BANK CARD",
+    text: "NEW CARD",
+  };
+
+  console.log(addCardHeader);
+
   return (
     <div className="top">
       <h1 className="top-header">E-WALLET</h1>
