2caa969a52c151b2f2db2f2c5afe196aebde4a6c
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-02-15 22:45:03 +0700

    Complete

--- e-wallet/src/components/AddCard/AddCard.jsx
+++ e-wallet/src/components/AddCard/AddCard.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 
 export default function AddCard() {
   const cardData = CreditCardsData[4];
+  const [cards, setCards] = useState([]);
 
   const newCard = {
     cardNumber: "",
